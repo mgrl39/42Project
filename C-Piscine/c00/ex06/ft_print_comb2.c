f7a0850824b6eb289cbd21a74d903e3e6d36f127
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 19:46:39 +0200

    Create ft_print_comb2.c

---
+++ C-Piscine/c00/ex06/ft_print_comb2.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar((i / 10) + '0');
+			ft_putchar((i % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((j / 10) + '0');
+			ft_putchar((j % 10) + '0');
+			if (i != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+}
