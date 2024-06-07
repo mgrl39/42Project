ac31205fa34a073daae343a21936c3655236a23a
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 19:44:49 +0200

    Create ft_print_numbers.c

---
+++ C-Piscine/c00/ex03/ft_print_numbers.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char	number;
+
+	number = 48;
+	while (number <= 57)
+	{
+		write(1, &number, 1);
+		number++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
+*/
