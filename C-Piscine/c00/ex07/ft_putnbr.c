3b660766c6a03374a327e0021fde823450782f17
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 19:47:24 +0200

    Create ft_putnbr.c

---
+++ C-Piscine/c00/ex07/ft_putnbr.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	ft_putchar(nb % 10 + '0');
+}
