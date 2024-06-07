51bb9de6ae132cb950c5ddaebe50db9fea92c931
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 20:23:49 +0200

    Create ft_strncat.c

---
+++ C-Piscine/c03/ex03/ft_strncat.c
@@ -0,0 +1,19 @@
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while (i < nb && src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
