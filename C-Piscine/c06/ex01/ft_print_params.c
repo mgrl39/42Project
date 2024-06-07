3f89e9a6bdf18ce9d40774874a28c3028c0a7b42
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 20:54:51 +0200

    Create ft_print_params.c

---
+++ C-Piscine/c06/ex01/ft_print_params.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
