8efd7ac8bbcbf0006e6dccf0629cdaa75bd7ecff
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 12:26:37 +0200

    Create ft_sqrt.c

---
+++ C-Piscine-Reloaded/ex14/ft_sqrt.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_sqrt(int nb)
+{
+	int	sqrt;
+	if (nb < 0)
+		return (0);
+	sqrt = 0;
+	while (sqrt * sqrt < nb)
+		sqrt++;
+	if (sqrt * sqrt == nb)
+		return (sqrt);
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+    int number, result;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <number>\n", argv[0]); // Informative error message
+        return (1); // Indicate error
+    }
+
+    number = atoi(argv[1]);
+    if (number < 0) {
+        printf("Square root is not defined for negative numbers.\n");
+        return (1); // Indicate error
+    }
+
+    result = ft_sqrt(number);
+
+    if (result == 0) {
+        printf("The number %d is not a perfect square.\n", number);
+    } else {
+        printf("The square root of %d is %d\n", number, result);
+    }
+
+    return (0); // Indicate successful execution
+}
