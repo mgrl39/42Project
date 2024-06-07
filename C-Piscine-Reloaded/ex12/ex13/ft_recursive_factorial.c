1bde82338f5529895e3cc58ed0246d2022150a8d
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 11:47:46 +0200

    Create ft_recursive_factorial.c

---
+++ C-Piscine-Reloaded/ex12/ex13/ft_recursive_factorial.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0 || nb == 1)
+		return (1);
+	else
+		return (nb * ft_recursive_factorial(nb - 1));
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <number>\n", argv[0]); // Informative error message
+        return (1); // Indicate error
+    }
+
+    int number;
+
+    number = atoi(argv[1]);
+
+    int factorial = ft_recursive_factorial(number);
+
+    printf("The factorial of %d is %d\n", number, factorial);
+
+    return (0); // Indicate successful execution
+}
