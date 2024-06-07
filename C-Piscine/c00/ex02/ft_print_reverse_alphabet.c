58d6f4b3664b9bd5158c5e48446263525d4e0f92
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 19:38:52 +0200

    Create ft_print_reverse_alphabet.c

---
+++ C-Piscine/c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write(1, &letter, 1);
+		letter--;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
+*/
