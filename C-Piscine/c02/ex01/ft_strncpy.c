a781edd11bbc9a9ee1ae184dc7ae8ff85ccac5f3
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 20:15:32 +0200

    Create ft_strncpy.c

---
+++ C-Piscine/c02/ex01/ft_strncpy.c
@@ -0,0 +1,18 @@
+/*  https://linux.die.net/man/3/strncpy */
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	count;
+
+	count = 0;
+	while (src[count] != '\0' && count < n)
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	while (count < n)
+	{
+		dest[count] = '\0';
+		count++;
+	}
+	return (dest);
+}
