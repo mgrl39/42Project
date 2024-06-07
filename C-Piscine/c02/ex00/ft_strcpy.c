b2b2b2d750a122b22d82f262744bf6659ff45447
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 20:09:58 +0200

    Create ft_strcpy.c

---
+++ C-Piscine/c02/ex00/ft_strcpy.c
@@ -0,0 +1,13 @@
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	count;
+
+	count = 0;
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
