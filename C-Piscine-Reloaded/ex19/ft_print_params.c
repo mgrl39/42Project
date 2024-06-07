2dea9e5fd4be195fdac106e53aba14ad825a3036
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 11:58:13 +0200

    Create ft_print_params.c

---
+++ C-Piscine-Reloaded/ex19/ft_print_params.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
