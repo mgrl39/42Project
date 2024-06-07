0305b9d011817c5a79faa5246aac3a0718ef6b19
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 20:48:01 +0200

    Create ft_find_next_prime.c

---
+++ C-Piscine/c05/ex07/ft_find_next_prime.c
@@ -0,0 +1,27 @@
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	i = 2;
+	if (nb <= 1)
+		return (0);
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (2);
+	i = nb;
+	while (!ft_is_prime(i))
+		i++;
+	return (i);
+}
