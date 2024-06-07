ac9b3fac195532f63e78544865a17a970d93ce0a
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-07 20:28:04 +0200

    Create ft_atoi.c

---
+++ C-Piscine/c04/ex03/ft_atoi.c
@@ -0,0 +1,28 @@
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		check_negative;
+	int		result;
+
+	check_negative = 1;
+	result = 0;
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			check_negative *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	result *= check_negative;
+	return (result);
+}
