6191f58f3a65f444822e76c345244ae73ec67359
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-08 18:32:10 +0200

    Update ft_print_numbers.c

--- C-Piscine-Reloaded/ex07/ft_print_numbers.c
+++ C-Piscine-Reloaded/ex07/ft_print_numbers.c
@@ -1,9 +1,6 @@
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_putchar(char c);
 
 void	ft_print_numbers(void)
 {
@@ -16,8 +13,14 @@ void	ft_print_numbers(void)
 		number++;
 	}
 }
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
 
 int	main(void)
 {
 	ft_print_numbers();
 }
+*/
