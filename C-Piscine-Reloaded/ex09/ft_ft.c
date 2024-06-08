f24e4e1b73cdf78bd11efecc030b80bfd6ccd505
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-08 18:35:02 +0200

    Update ft_ft.c

--- C-Piscine-Reloaded/ex09/ft_ft.c
+++ C-Piscine-Reloaded/ex09/ft_ft.c
@@ -1,11 +1,12 @@
+/*
 #include <unistd.h>
 #include <stdio.h>
-
+*/
 void	ft_ft(int *nbr)
 {
 	*nbr = 42;
 }
-
+/*
 int	main(void)
 {
 	int	a;
@@ -15,3 +16,4 @@ int	main(void)
 	printf("El valor de la variable es: %d\n", a);
 	return (0);
 }
+*/
