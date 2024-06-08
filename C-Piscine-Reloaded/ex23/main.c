5b3bfc9a799145bdaaa0d66ff2cdfac10873a4f8
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-09 01:29:04 +0200

    Update main.c

--- C-Piscine-Reloaded/ex23/main.c
+++ C-Piscine-Reloaded/ex23/main.c
@@ -12,3 +12,43 @@ int main(void)
   set_point(&point);
   return (0);
 }
+
+// Main with ft_putnbr.c and putchar.c to try
+/* 
+
+void  ft_putchar(char c)
+{
+  write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+	}
+	ft_putchar(nb % 10 + '0');
+}
+
+int main()
+{
+	t_point point;
+
+	set_point(&point);
+	
+	ft_putnbr(point.x);
+	ft_putchar('\0');
+	ft_putnbr(point.y);
+	return 0;
+}
+*/
