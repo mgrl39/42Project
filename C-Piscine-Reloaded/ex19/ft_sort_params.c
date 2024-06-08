5db2108d2017a9821ab3f89d0054c92bcf51b3f2
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-08 21:52:09 +0200

    Update ft_sort_params.c

--- C-Piscine-Reloaded/ex19/ft_sort_params.c
+++ C-Piscine-Reloaded/ex19/ft_sort_params.c
@@ -1,43 +1,64 @@
-#include <unistd.h>
+//#include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c);
+
+int	ft_strcmp(char *s1, char *s2)
 {
-    write(1, &c, 1);
+	int	count;
+
+	count = 0;
+	while (s1[count] == s2[count] && s1[count] != '\0')
+		count++;
+	return (s1[count] - s2[count]);
 }
 
-int ft_strcmp(char *s1, char *s2)
+void	ft_sort_params(int argc, char *argv[])
 {
-    int count;
+	int		i;
+	int		j;
+	char	*temp;
 
-    count = 0;
-    while (s1[count] == s2[count] && s1[count] != '\0')
-    count++;
-    return (s1[count]-s2[count]);
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				temp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+/*
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
 
-int main(int argc, char *argv[]) {
-  int i = 1;
-  while (i < argc - 1) {
-    int j = i + 1;
-    while (j < argc) {
-      if (ft_strcmp(argv[i], argv[j]) > 0) {
-        char *temp = argv[i];
-        argv[i] = argv[j];
-        argv[j] = temp;
-      }
-      j++;
-    }
-    i++;
-  }
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
 
-    i = 1;
-  while (i < argc) {
-    int j = 0;
-    while (argv[i][j] != '\0') {
-      ft_putchar(argv[i][j]);
-      j++;
-    }
-    ft_putchar('\n');
-    i++;
-  }
+	ft_sort_params(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
 }
+*/
