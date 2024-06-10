9f3bc00dc3a0ed6f03f97968975f89639776fcd0
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-10 15:03:30 +0200

    Update ft_iterative_factorial.c

--- C-Piscine-Reloaded/ex12/ft_iterative_factorial.c
+++ C-Piscine-Reloaded/ex12/ft_iterative_factorial.c
@@ -1,9 +1,3 @@
-
-/*
-#include <stdio.h>
-#include <stdlib.h> // for atoi
-*/
-
 int	ft_iterative_factorial(int nb)
 {
 	int	result;
@@ -18,27 +12,3 @@ int	ft_iterative_factorial(int nb)
 	}
 	return (result);
 }
-/*
-int main(int argc, char *argv[])
-{
-    int number, factorial;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <number>\n", argv[0]); // Print usage message
-        return (1); // Indicate error
-    }
-
-    number = atoi(argv[1]); // Convert string argument to integer
-
-    if (number < 0) {
-        printf("Factorial is not defined for negative numbers.\n");
-        return (1); // Indicate error
-    }
-
-    factorial = ft_iterative_factorial(number);
-
-    printf("The factorial of %d is %d\n", number, factorial);
-
-    return (0); // Indicate successful execution
-}
-*/
