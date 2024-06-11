8690514157bf4bbe19d43ef0c747d83cf56262b0
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-11 16:48:56 +0200

    Update ft_isalnum.c

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -5,8 +5,8 @@ int	ft_isalnum(char c)
 	if ((c >= 48 && c <= 57)
 		|| (c >= 65 && c <= 90)
 		|| (c >= 97 && c <= 122))
-		return (0);
-	return (1);
+		return (1);
+	return (0);
 }
 
 void	ft_putchar(char c)
