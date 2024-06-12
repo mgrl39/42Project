01841e63451b1d41f00ee938365a4543e810a91a
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-12 12:04:46 +0200

    Update ft_memcmp.c

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -1,17 +1,17 @@
-#include "libft.h"
-
-int ft_memcmp(void const *s1, void const *s2, size_t n)
+void	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    unsigned char *str1 = (unsigned char *)s1;
-    unsigned char *str2 = (unsigned char *)s2;
-    size_t i = 0; // Contador
-    
-    while (i < n)
-    {
-        if (str1[i] != str2[i])
-            return (str1[i] - str2[i]);
-        
-        i++;
-    }
-    return (0);
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
 }
