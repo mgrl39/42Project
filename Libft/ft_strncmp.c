151aa1032eea1dd107e86feee772b30f8c210a7a
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-13 15:48:51 +0200

    Create ft_strncmp.c

---
+++ Libft/ft_strncmp.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+int	strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (str1[i] != '\0' && str1[i] == str2[i] && i < n - 1)
+		i++;
+	return (str1[i] - str2[i]);
+}
