8f90496ce41c975048aae26b0ceed527aec8a812
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-13 02:05:30 +0200

    Update ft_memcmp.c

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -1,3 +1,20 @@
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
 void	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*str1;
