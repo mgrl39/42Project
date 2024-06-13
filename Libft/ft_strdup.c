f93b7b87d37e552e17cbea1ef39cedef4671337e
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-13 16:43:57 +0200

    Create ft_strdup.c

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
