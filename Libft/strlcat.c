5ca63f0d144bb993495ea292adedc9ddfa172f68
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-15 16:40:08 +0200

    Create strlcat.c

---
+++ Libft/strlcat.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+size_t	strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len_dest;
+	size_t	len_src;
+	size_t	i;
+
+	len_dest = 0;
+	len_src = 0;
+	i = 0;
+	while (dest[len_dest] != '\0')
+		len_dest++;
+	while (src[len_src] != '\0')
+		len_src++;
+	if (size <= len_dest)
+		len_src += size;
+	else
+		len_src += len_dest;
+	while (src[i] != '\0' && (len_dest + 1) < size - 1)
+	{
+		dest[len_dest + i] = src[i];
+		i++;
+	}
+	dest[len_dest + i] = '\0';
+	return (len_src);
+}
