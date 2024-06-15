17648a1680fa913f3ba1245aa3937b51bccd5e10
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-15 19:38:54 +0200

    Update ft_memcmp.c

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -15,7 +15,7 @@ void	ft_putstr(char *str)
 	}
 }
 
-void	ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*str1;
 	unsigned char	*str2;
