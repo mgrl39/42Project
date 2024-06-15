ad1a6b9fce866036fa415ec1d30b3e865d6cfc27
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-15 19:55:23 +0200

    Update ft_memcmp.c

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -32,3 +32,24 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
+
+
+#include <stdio.h>
+
+int main() {
+    char mem1[] = "abcdef";
+    char mem2[] = "abcdez";
+    
+    int result = ft_memcmp(mem1, mem2, 6);
+    
+    if (result < 0) {
+        printf("mem1 is less than mem2\n");
+    } else if (result > 0) {
+        printf("mem1 is greater than mem2\n");
+    } else {
+        printf("mem1 is equal to mem2\n");
+    }
+    
+    return 0;
+}
+
