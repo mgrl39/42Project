7539c3be2d6585383c89da11fc7d38083f8bdd5f
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-16 13:34:16 +0200

    Create ft_memcpy.c

---
+++ Libft/ft_memcpy.c
@@ -0,0 +1,18 @@
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*tmp_dest;
+	unsigned char	*tmp_src;
+	size_t			i;
+
+	if (!dest && !src)
+		return (dest);
+	tmp_dest = (unsigned char *)dest;
+	tmp_src = (unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		tmp_dest[i] = tmp_src[i];
+		i++;
+	}
+	return (dest);
+}
