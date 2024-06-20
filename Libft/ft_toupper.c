6dbf5bff7c05863152ae1170e4f07305529cfb7a
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 20:25:07 +0200

    Update ft_toupper.c

--- Libft/ft_toupper.c
+++ Libft/ft_toupper.c
@@ -1,25 +1,8 @@
 #include "libft.h"
-#include <unistd.h>
 
 int	ft_toupper(int c)
 {
 	if (c >= 97 && c <= 122)
 		c -= 32;
 	return (c);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-int	main(int argc, char *argv[])
-{
-	if (argc == 2)
-	{
-		argv[1][0] = ft_toupper(argv[1][0]);
-		ft_putchar(argv[1][0]);
-		ft_putchar('\n');
-	}
-	return (0);
 }
