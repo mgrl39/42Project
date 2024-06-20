a1373713dbc1c7f7e90b2a800d2f1f98a4b29d3e
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 21:22:00 +0200

    Create ft_strrchr.c

---
+++ Libft/ft_strrchr.c
@@ -0,0 +1,28 @@
+#include "libft.h"
+
+/* ft_strrchr function that returns a pointer to the last
+ * occurrence of character 'c' in string 's'.
+ * If 'c' is not found, it returns NULL.
+ */
+
+char	*ft_strrchr(const char *s, int c)
+{
+	unsigned char	*str;
+	unsigned char	uc;
+	char			*res;
+	size_t			i;
+
+	str = (unsigned char *)s;
+	uc = (unsigned char)c;
+	res = NULL;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == uc)
+			res = (char *)&str[i];
+		i++;
+	}
+	if (str[i] == uc)
+		res = (char *)&str[i];
+	return (res);
+}
