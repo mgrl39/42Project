ef1b34577ab9185b38404a8f6c7840622c304196
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 21:25:41 +0200

    Update ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -1,5 +1,13 @@
 #include "libft.h"
 
+/*
+ * The  strcmp()  function  compares the two strings s1 and s2.
+ * The comparison is done using  unsigned characters.
+ * The strncmp() function is similar to strcmp(), except it compares
+ * only the first (at most) n bytes of s1 and s2.
+ * Return an integer less than, equal, or greather than 0.
+ */
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned char	*str1;
