9ca7955bf046f6bf14b0034275f17d6214e37e2f
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 21:44:00 +0200

    Update ft_bzero.c

--- Libft/ft_bzero.c
+++ Libft/ft_bzero.c
@@ -13,24 +13,3 @@ void	ft_bzero(void *s, size_t n)
 		i++;
 	}
 }
-
-int	main(int argc, char *argv[])
-{
-	char	*str;
-	int		n;
-
-	if (argc != 3)
-	{
-		ft_putstr("Usage: ");
-		ft_putstr(argv[0]);
-		ft_putstr(" <string> <number>\n");
-		return (1);
-	}
-	str = argv[1];
-	n = ft_atoi(argv[2]);
-	ft_bzero(str, n);
-	ft_putstr("Resulting string after ft_bzero: ");
-	ft_putstr(str);
-	ft_putchar('\n');
-	return (0);
-}
