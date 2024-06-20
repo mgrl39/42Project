f10da665f34206b568ca6b36295729b16022e17e
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 19:16:55 +0200

    Update ft_isdigit.c

--- Libft/ft_isdigit.c
+++ Libft/ft_isdigit.c
@@ -1,58 +1,9 @@
-
 #include "libft.h"
+
+// ft_isalpha function that checks if a character is a digit
+// Returns 1 if c is a digit (from 0 to 9), 0 otherwise
 
 int	ft_isdigit(int c)
 {
-	if (c >= 48 && c <= 57)
-		return (1);
-	return (0);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-void	ft_putstr(char *str)
-{
-	int	i;
-	int	j;
-
-	j = 0;
-	i = ft_strlen(str);
-	while (j < i)
-	{
-		ft_putchar(str[j]);
-		j++;
-	}
-}
-
-int	main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		ft_putstr(argv[0]);
-		ft_putstr(" <character>\n");
-		return (1);
-	}
-	if (ft_strlen(argv[1]) > 1)
-	{
-		ft_putstr("Error, only one character\n");
-		return (1);
-	}
-	if (ft_isdigit(argv[1][0]))
-		ft_putstr("Is a digit\n");
-	else
-		ft_putstr("Is not a digit\n");
-	return (0);
+	return (c >= 48 && c <= 57);
 }
