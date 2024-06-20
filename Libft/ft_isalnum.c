c1e9debe764664b59f1e8013aacd5e50b47b928c
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 19:24:08 +0200

    Update ft_isalnum.c

--- Libft/ft_isalnum.c
+++ Libft/ft_isalnum.c
@@ -1,57 +1,11 @@
 #include "libft.h"
 
-int	ft_isalnum(char c)
+// ft_isalnum function that checks if a character is alphabetic or a digit.
+// Returns 1 if c is a letter (uppercase or lowercase) or digit, 0 otherwise.
+
+int	ft_isalnum(int c)
 {
-	if ((c >= 48 && c <= 57)
+	return ((c >= 48 && c <= 57)
 		|| (c >= 65 && c <= 90)
-		|| (c >= 97 && c <= 122))
-		return (1);
-	return (0);
-}
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-int	main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		ft_putstr(argv[0]);
-		ft_putstr(" <character>\n");
-		return (1);
-	}
-	if (ft_strlen(argv[1]) > 1)
-	{
-		ft_putstr("Error, only one character\n");
-		return (1);
-	}
-	if (!ft_isalnum(argv[1][0]))
-		ft_putstr("Is a alphanumeric character> a letter or digit.\n");
-	else
-		ft_putstr("Is not a alphanumeric character.\n");
-	return (0);
+		|| (c >= 97 && c <= 122));
 }
