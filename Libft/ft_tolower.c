378a63757640f8eb8d25a2f4402adc12e5d635a0
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 20:31:25 +0200

    Update ft_tolower.c

--- Libft/ft_tolower.c
+++ Libft/ft_tolower.c
@@ -1,8 +1,12 @@
 #include "libft.h"
 
+// ft_tolower function that converts an uppercase letter to lowercase.
+// Returns the lowercase equivalent of the input character 'c',
+// or the character itself if 'c' is not an uppercase letter.
+
 int	ft_tolower(int c)
 {
 	if (c >= 65 && c <= 90)
-		return (c + 32);
+		c += 32;
 	return (c);
 }
