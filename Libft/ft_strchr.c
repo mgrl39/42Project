97c85dacb3324f91bdaa563626b09213a67cc71d
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 21:18:33 +0200

    Create ft_strchr.c

---
+++ Libft/ft_strchr.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+/* ft_strchr function that returns a pointer to the first occurrence 
+ * of character 'c' in string 's'.
+ * If 'c' is not found, it returns a pointer to the null terminator
+ * at the end of 's'.
+ */
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	*str;
+	unsigned char	uc;
+	size_t			i;
+
+	str = (unsigned char *)s;
+	uc = (unsigned char)c;
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == uc)
+			return ((char *)&str[i]);
+		i++;
+	}
+	if (uc == '\0')
+		return ((char *)&str[i]);
+	return (NULL);
+}
