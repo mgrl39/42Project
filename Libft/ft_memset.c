1a9af02bd849f75937cf410edfa4070ccbbf89f4
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 21:36:43 +0200

    Update ft_memset.c

--- Libft/ft_memset.c
+++ Libft/ft_memset.c
@@ -1,5 +1,9 @@
 #include "libft.h"
 
+// ft_memset function fills the firsst n bytes of the memory area pointed to
+// by s with the constant byte c.
+// Returns a pointer to the memory area s.
+
 void	*ft_memset(void *s, int c, size_t n)
 {
 	size_t				i;
