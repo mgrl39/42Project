4b7f5b819bbf889dcbcdf36bef25e88dc31adf3f
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-20 21:14:52 +0200

    Update ft_strlen.c

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -1,3 +1,9 @@
+#include "libft.h"
+
+// ft_strlen function that calculates the length of a string
+// Returns the number of characters in the string str 
+// excluding the null terminator '\0'
+
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
