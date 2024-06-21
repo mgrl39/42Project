d81182cf0a4feb1fcaee21a3e618564088c32282
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-22 01:29:42 +0200

    Create ft_putnbr.c

---
+++ Libft/ft_putnbr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	if (nb < 10)
+		ft_putchar(nb + 48);
+}
