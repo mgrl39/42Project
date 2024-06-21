6a168cfa0124e816c8a6d609bccaa463c5e666e1
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-22 01:31:46 +0200

    Update ft_strlcpy.c

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -1,25 +1,5 @@
 #include "libft.h"
 
-/*
-#include <string.h>
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-int	main(void)
-{
-	char *str1;
-	char *str2;
-	size_t size;
-
-	str1 = "Buenos";
-	str2 = "Dias";
-	size = 3;
-	size_t value = strlcpy(str1, str2, size);
-	ft_putnbr(value);
-}
-*/
 size_t	strlcpy(char *dest, const char *src, size_t size)
 {
 
