047ff2fa7acaf12e359ddb65b8c7db3dc7268fdc
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-21 19:15:30 +0200

    Add files via upload

---
+++ Libft/ft_memmove.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char		*p_dest;
+	const unsigned char	*p_src;
+	size_t				i;
+	if (dest == src || n == 0)
+		return (dest);
+	p_dest = (unsigned char *)dest;
+	p_src = (const unsigned char *)src;
+	if (dest < src)
+	{
+		i = 0;
+		while (i < n)
+		{
+			p_dest[i] = p_src[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (n > 0)
+		{
+			n--;
+			p_dest[n] = p_src[n];
+		}
+	}
+	return (dest);
+}
