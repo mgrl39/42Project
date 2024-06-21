9d072b20ebf5d4ec317ba6c9afed1c080a5da3d1
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-22 01:29:24 +0200

    Update ft_strlcpy.c

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -1,28 +1,11 @@
 #include "libft.h"
 
+/*
 #include <string.h>
-
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-
-void	ft_putnbr(int nb)
-{
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		nb = -nb;
-	}
-	if (nb > 10)
-	{
-		ft_putnbr(nb / 10);
-		nb = nb % 10;
-	}
-	if (nb < 10)
-		ft_putchar(nb + 48);
-}
-
 
 int	main(void)
 {
@@ -36,6 +19,7 @@ int	main(void)
 	size_t value = strlcpy(str1, str2, size);
 	ft_putnbr(value);
 }
+*/
 size_t	strlcpy(char *dest, const char *src, size_t size)
 {
 
