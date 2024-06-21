af401622afc5855100f7faa07ab5a6caddc65ae3
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-21 23:22:17 +0200

    Update ft_isprint.c

--- Libft/ft_isprint.c
+++ Libft/ft_isprint.c
@@ -1,5 +1,8 @@
 #include "libft.h"
 
+// ft_isprint function that checks if a character is a printable character.
+// Returns 1 if c is a printable character, 0 otherwise.
+
 int	ft_isprint(int c)
 {
 	return (c >= 32 && c <= 126);
