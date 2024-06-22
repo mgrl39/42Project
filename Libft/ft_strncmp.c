f965ef0102985abec1d91d68ad1509dca78cecf4
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-22 14:45:59 +0200

    Update ft_strncmp.c

--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -14,12 +14,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*str2;
 	size_t			i;
 
+	if (n == 0)
+		return (0);
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (str1[i] != '\0' && str1[i] == str2[i] && i < n - 1)
+	while (str1[i] != '\0' && str1[i] == str2[i] && i < (n - 1))
 		i++;
 	return (str1[i] - str2[i]);
 }
