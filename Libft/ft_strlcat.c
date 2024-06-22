691ee9280175572d9ca3743a3697250af88106b3
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-22 19:48:38 +0200

    Update ft_strlcat.c

--- Libft/ft_strlcat.c
+++ Libft/ft_strlcat.c
@@ -25,3 +25,33 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dest[len_dest + i] = '\0';
 	return (len_src);
 }
+
+/*
+#include "libft.h"
+
+// nns si esta acabado mirar gihtub.
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	len_dst;
+	size_t	len_src;
+	size_t	i;
+
+	len_dst = 0;
+	len_src = 0;
+	i = 0;
+	while (dst[len_dst] != '\0')
+		len_dst++;
+	while (src[len_src] != '\0')
+		len_src++;
+	if (size <= len_dst)
+		return (len_src + size);
+	while (src[i] != '\0' && (len_dst + i + 1) < size)
+	{
+		dst[len_dst + i] = src[i];
+		i++;
+	}
+	if (len_dst + i < size)
+		dst[len_dst + i] = '\0';
+	return (len_dst + len_src);
+}
+*/
