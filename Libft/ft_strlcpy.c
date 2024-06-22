8b8631051355656fc80d24b37acd8a442e2f53ce
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-22 14:56:39 +0200

    Update ft_strlcpy.c

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -1,6 +1,31 @@
 #include "libft.h"
 
-size_t	strlcpy(char *dest, const char *src, size_t size)
-{
+/*
+ * The strlcpy() function copies up to (size - 1) characters from the
+ * NUL-terminated string src to dst, NUL-terminating the result.
+ * It returns the total length of the string they tried to create.
+ * For ft_strlcpy() that means the length of src
+ */
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	len_src;
+	size_t	i;
+
+	if (!dst || !src)
+		return (0);
+	len_src = 0;
+	while (src[len_src] != '\0')
+		len_src++;
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (len_src);
 }
