3e861903dabda9c28dfa8385ae4f189e3714d5a1
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-22 19:49:53 +0200

    Update ft_strnstr.c

--- Libft/ft_strnstr.c
+++ Libft/ft_strnstr.c
@@ -13,7 +13,7 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	j;
 
 	i = 0;
-	if (!*little)
+	if (!little)
 		return ((char *)big);
 	while (big[i] != '\0' && i < len)
 	{
