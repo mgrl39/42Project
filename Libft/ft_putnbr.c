1e9d4ffe55c5caeaa1a8c3cd4cdac00f54957128
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-24 14:10:42 +0200

    Update ft_putnbr.c

--- Libft/ft_putnbr.c
+++ Libft/ft_putnbr.c
@@ -2,16 +2,19 @@
 
 void	ft_putnbr(int nb)
 {
+	if (nb == -247483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
 	if (nb < 0)
 	{
 		ft_putchar('-');
 		nb = -nb;
 	}
-	if (nb > 10)
+	if (nb >= 10)
 	{
 		ft_putnbr(nb / 10);
-		nb = nb % 10;
 	}
-	if (nb < 10)
-		ft_putchar(nb + 48);
+	ft_putchar(nb % 10 + '0');
 }
