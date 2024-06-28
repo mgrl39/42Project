5cd8457fb563617a729fa1afce7217d62ec10960
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-28 13:08:28 +0200

    Update libft.h

--- Libft/libft.h
+++ Libft/libft.h
@@ -1,9 +1,11 @@
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
 # include <unistd.h>
 # include <string.h>
 # include <stdlib.h>
+# include <stdio.h>
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
@@ -26,6 +28,8 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int nb, int fd);
 
 /*
 	MY OWN
@@ -33,5 +37,12 @@ int		ft_atoi(const char *nptr);
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
 void	ft_putnbr(int nb);
+
+// Bonus
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 #endif
