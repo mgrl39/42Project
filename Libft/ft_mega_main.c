abc55dbe3a4ae194e11803310473e03e1bee4105
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-07-14 17:31:49 +0200

    Update ft_mega_main.c

--- Libft/ft_mega_main.c
+++ Libft/ft_mega_main.c
@@ -1,3 +1,4 @@
+
 #include "libft.h"
 #include <ctype.h>
 #include <stddef.h>
@@ -76,36 +77,42 @@ static void	ft_is_tester(char a, int fd)
 	ft_putstr_fd("a = ", 1);
 	ft_putchar_fd(a, 1);
 	ft_putchar_fd('\n', 1);
+	if (!ft_isalnum(a) == !isalnum(a))
+		ft_putstr_fd("✅ ", 1);
+	else
+		ft_putstr_fd("❌ ", 1);
 	ft_putstr_fd("ft_isalnum(a): ", 1);
 	ft_putnbr_fd(ft_isalnum(a), 1);
-	if (!ft_isalnum(a) == !isalnum(a))
-		ft_putendl_fd(" 👍", 1);
+	ft_putchar_fd('\n', 1);
+	if (!ft_isalpha(a) == !isalpha(a))
+		ft_putstr_fd("✅ ", 1);
 	else
-		ft_putendl_fd(" 👎", 1);
+		ft_putstr_fd("❌ ", 1);
 	ft_putstr_fd("ft_isalpha(a): ", 1);
 	ft_putnbr_fd(ft_isalpha(a), 1);
-	if (!ft_isalpha(a) == !isalpha(a))
-		ft_putendl_fd(" 👍", 1);
+	ft_putchar_fd('\n', 1);
+	if (!ft_isascii(a) == !isascii(a))
+		ft_putstr_fd("✅ ", 1);
 	else
-		ft_putendl_fd(" 👎", 1);
+		ft_putstr_fd("❌ ", 1);
 	ft_putstr_fd("ft_isascii(a): ", 1);
 	ft_putnbr_fd(ft_isascii(a), 1);
-	if (!ft_isascii(a) == !isascii(a))
-		ft_putendl_fd(" 👍", 1);
+	ft_putchar_fd('\n', 1);
+	if (!ft_isdigit(a) == !isdigit(a))
+		ft_putstr_fd("✅ ", 1);
 	else
-		ft_putendl_fd(" 👎", 1);
+		ft_putstr_fd("❌ ", 1);
 	ft_putstr_fd("ft_isdigit(a): ", 1);
 	ft_putnbr_fd(ft_isdigit(a), 1);
-	if (!ft_isdigit(a) == !isdigit(a))
-		ft_putendl_fd(" 👍", 1);
+	ft_putchar_fd('\n', 1);
+	if (!ft_isprint(a) == !isprint(a))
+		ft_putstr_fd("✅ ", 1);
 	else
-		ft_putendl_fd(" 👎", 1);
+		ft_putstr_fd("❌ ", 1);
 	ft_putstr_fd("ft_isprint(a): ", 1);
 	ft_putnbr_fd(ft_isprint(a), 1);
-	if (!ft_isprint(a) == !isprint(a))
-		ft_putendl_fd(" 👍", 1);
-	else
-		ft_putendl_fd(" 👎", 1);
+	ft_putchar_fd('\n', 1);
+
 }
 
 static void	ft_to_tester(char a, int fd)
@@ -114,20 +121,22 @@ static void	ft_to_tester(char a, int fd)
 	ft_putstr_fd("a = ", 1);
 	ft_putchar_fd(a, fd);
 	ft_putchar_fd('\n', fd);
+	if (ft_tolower(a) == tolower(a))
+		ft_putstr_fd("✅ ", 1);
+	else
+		ft_putstr_fd("❌ ", 1);
 	ft_putstr_fd("ft_tolower(a): ", fd);
 	a = ft_tolower(a);
 	ft_putchar_fd(a, fd);
+	ft_putchar_fd('\n', fd);
 	if (ft_tolower(a) == tolower(a))
-		ft_putendl_fd(" 👍", 1);
+		ft_putstr_fd("✅ ", 1);
 	else
-		ft_putendl_fd(" 👎", 1);
+		ft_putstr_fd("❌ ", 1);
 	ft_putstr_fd("ft_toupper(a): ", fd);
 	a = ft_toupper(a);
 	ft_putchar_fd(a, fd);
-	if (ft_tolower(a) == tolower(a))
-		ft_putendl_fd(" 👍", 1);
-	else
-		ft_putendl_fd(" 👎", 1);
+	ft_putchar_fd('\n', fd);
 }
 
 static	void	ft_mem_tester(void)
@@ -143,31 +152,31 @@ static	void	ft_mem_tester(void)
 	ft_memset(mine_memset, 50, ft_strlen(mine_memset));
 	memset(original_memset, 50, strlen(original_memset));
 	if (memcmp(mine_memset,original_memset,size))
-		ft_putendl_fd("ft_memset 👍", 1);
+		ft_putendl_fd("✅ ft_memset ", 1);
 	else
-		ft_putendl_fd("ft_memset 👎", 1);
+		ft_putendl_fd("❌ ft_memset ", 1);
 	// =================================================================
 	char	mine_bzero[size];
 	char	original_bzero[size];
 	ft_bzero(mine_bzero, ft_strlen(mine_bzero));
 	bzero(original_bzero, strlen(original_bzero));
 	if (memcmp(mine_bzero,original_bzero, size))
-		ft_putendl_fd("ft_bzero 👍", 1);
+		ft_putendl_fd("✅ ft_bzero ", 1);
 	else
-		ft_putendl_fd("ft_bzero 👎", 1);
+		ft_putendl_fd("❌ ft_bzero ", 1);
 	// =================================================================
 	if (memcmp(ft_calloc(10,10),calloc(10,10),100) == 0)
-		ft_putendl_fd("ft_calloc 👍", 1);
+		ft_putendl_fd("✅ ft_calloc ", 1);
 	else
-		ft_putendl_fd("ft_calloc 👎", 1);
+		ft_putendl_fd("❌ ft_calloc", 1);
 	// =================================================================
 
 	if ( \
 			memcmp(mine_bzero, original_bzero, size) == \
 			ft_memcmp(mine_bzero, original_bzero, size))
-		ft_putendl_fd("ft_memcmp 👍", 1);
+		ft_putendl_fd("✅ ft_memcmp ", 1);
 	else
-		ft_putendl_fd("ft_memcmp 👎", 1);
+		ft_putendl_fd("❌ ft_memcmp ", 1);
 	// =================================================================
 	char	memcpy_src[size];
 	char	mine_memcpy[size];
@@ -175,9 +184,9 @@ static	void	ft_mem_tester(void)
 	
 	ft_memcpy(mine_memcpy, memcpy_src, size);
 	if (memcmp(mine_memcpy, memcpy_src, size) == 0)
-		ft_putendl_fd("ft_memcpy 👍", 1);
+		ft_putendl_fd("✅ ft_memcpy", 1);
 	else
-		ft_putendl_fd("ft_memcpy 👎", 1);
+		ft_putendl_fd("❌ ft_memcpy", 1);
 	// =================================================================
 	char	memmove_src[size];
 	char	mine_memmove[size];
@@ -185,15 +194,15 @@ static	void	ft_mem_tester(void)
 
 	ft_memmove(mine_memmove, memmove_src, size);
 	if (memcmp(mine_memmove, memmove_src, size) == 0)
-		ft_putendl_fd("ft_memmove 👍", 1);
+		ft_putendl_fd("✅ ft_memmove", 1);
 	else
-		ft_putendl_fd("ft_memmove 👎", 1);
+		ft_putendl_fd("❌ ft_memmove", 1);
 	// =================================================================
 	memmove_src[7] = 'a';
 	if (ft_memchr(memmove_src, 'a', size) == memchr(memmove_src, 'a', size))
-		ft_putendl_fd("ft_memchr 👍", 1);
+		ft_putendl_fd("✅ ft_memchr", 1);
 	else
-		ft_putendl_fd("ft_memchr 👎", 1);
+		ft_putendl_fd("❌ ft_memchr", 1);
 	// =================================================================
 
 }
@@ -205,15 +214,15 @@ static void	ft_conv_tester()
 	s = "10";
 	ft_putendl_fd("CONVERSION=====================================", 1);
 	if (atoi("10") == ft_atoi("10"))
-		ft_putendl_fd("ft_atoi 👍", 1);
+		ft_putendl_fd("✅ ft_atoi", 1);
 	else
-		ft_putendl_fd("ft_atoi 👎", 1);
+		ft_putendl_fd("❌ ft_atoi", 1);
 
 	// =================================================================
 	if (strcmp(ft_itoa(10), s) == 0)
-		ft_putendl_fd("ft_itoa 👍", 1);
+		ft_putendl_fd("✅ ft_itoa", 1);
 	else
-		ft_putendl_fd("ft_itoa 👎", 1);
+		ft_putendl_fd("❌ ft_itoa ", 1);
 
 }
 
@@ -231,6 +240,17 @@ void	to_uppercase_ptr(unsigned int i, char *c)
 	if (*c >= 'a' && *c <= 'z')
 		*c = *c - ('a' - 'A');
 
+}
+
+static int compare_split_arrays(char **expected, char **result) {
+    int i = 0;
+    while (expected[i] != NULL && result[i] != NULL) {
+        if (strcmp(expected[i], result[i]) != 0) {
+            return 0; 
+        }
+        i++;
+    }
+    return (expected[i] == NULL && result[i] == NULL);
 }
 
 static void	ft_str_tester()
@@ -251,65 +271,56 @@ static void	ft_str_tester()
 	char w3[10];
 	size_t	result_std = strlcpy(w3, w1, nsize);
 	if (strcmp(w2,w3) == 0 && (result == result_std))
-		ft_putendl_fd("ft_strlcpy 👍", 1);
+		ft_putendl_fd("✅ ft_strlcpy", 1);
 	else
-		ft_putendl_fd("ft_strlcpy 👎", 1);
+		ft_putendl_fd("❌ ft_strlcpy", 1);
 	// =================================================================
 	if (ft_strlcat(dst, str, size) == strlcat(dst, str, size))
-		ft_putendl_fd("ft_strlcat 👍", 1);
+		ft_putendl_fd("✅ ft_strlcat", 1);
 	else
-		ft_putendl_fd("ft_strlcat 👎", 1);
+		ft_putendl_fd("❌ ft_strlcat", 1);
 	// =================================================================
 	if (ft_strchr(str, 111) == strchr(str, 111))
-		ft_putendl_fd("ft_strchr 👍", 1);
+		ft_putendl_fd("✅ ft_strchr", 1);
 	else
-		ft_putendl_fd("ft_strchr 👎", 1);
+		ft_putendl_fd("❌ ft_strchr", 1);
 
 	// =================================================================
 	if (ft_strrchr(str, 111) == strrchr(str, 111))
-		ft_putendl_fd("ft_strrchr 👍", 1);
+		ft_putendl_fd("✅ ft_strrchr", 1);
 	else
-		ft_putendl_fd("ft_strrchr 👎", 1);
-	// =================================================================
-	if (ft_strnstr(str, dst, 12) == strnstr(str, dst, 12))
-		ft_putendl_fd("ft_strnstr 👍", 1);
-	else
+		ft_putendl_fd("❌ ft_strrchr", 1);
 	// =================================================================
 	if (ft_strlcat(dst, str, size) == strlcat(dst, str, size))
-		ft_putendl_fd("ft_strlcat 👍", 1);
+		ft_putendl_fd("✅ ft_strlcat", 1);
 	else
-		ft_putendl_fd("ft_strlcat 👎", 1);
+		ft_putendl_fd("❌ ft_strlcat", 1);
 	// =================================================================
 	if (ft_strchr(str, 111) == strchr(str, 111))
-		ft_putendl_fd("ft_strchr 👍", 1);
+		ft_putendl_fd("✅ ft_strchr", 1);
 	else
-		ft_putendl_fd("ft_strchr 👎", 1);
+		ft_putendl_fd("❌ ft_strchr", 1);
 
 	// =================================================================
-	if (ft_strrchr(str, 111) == strrchr(str, 111))
-		ft_putendl_fd("ft_strrchr 👍", 1);
-	else
-		ft_putendl_fd("ft_strrchr 👎", 1);
-	// =================================================================
 	if (ft_strnstr(str, dst, 12) == strnstr(str, dst, 12))
-		ft_putendl_fd("ft_strnstr 👍", 1);
+		ft_putendl_fd("✅ ft_strnstr", 1);
 	else
-		ft_putendl_fd("ft_strnstr 👎", 1);
+		ft_putendl_fd("❌ ft_strnstr", 1);
 	// =================================================================
 	if (!ft_strncmp(ft_strdup(str), strdup(str), ft_strlen(str)))
-		ft_putendl_fd("ft_strdup 👍", 1);
+		ft_putendl_fd("✅ ft_strdup", 1);
 	else
-		ft_putendl_fd("ft_strdup 👎", 1);
+		ft_putendl_fd("❌ ft_strdup", 1);
 	// =================================================================
 	if (!strcmp(ft_strtrim("123hola321","123"), "hola"))
-		ft_putendl_fd("ft_strtrim 👍", 1);
+		ft_putendl_fd("✅ ft_strtrim", 1);
 	else
-		ft_putendl_fd("ft_strtrim 👎", 1);
+		ft_putendl_fd("❌ ft_strtrim", 1);
 	// =================================================================
 	if (!strcmp(ft_strjoin("Hola","Mundo!"), "HolaMundo!"))
-		ft_putendl_fd("ft_strjoin 👍", 1);
+		ft_putendl_fd("✅ ft_strjoin ", 1);
 	else
-		ft_putendl_fd("ft_strjoin 👎", 1);
+		ft_putendl_fd("❌ ft_strjoin", 1);
 	// =================================================================
 	char *strmapi_str = "Hola";
 	char *strmapi_expected =  "HOLA";
@@ -317,41 +328,60 @@ static void	ft_str_tester()
 
 	strmapi_result = ft_strmapi(strmapi_str, &to_uppercase);
 	if (!strcmp(strmapi_result, strmapi_expected)) 
-		ft_putendl_fd("ft_strmapi 👍", 1);
+		ft_putendl_fd("✅ ft_strmapi", 1);
 	else
-		ft_putendl_fd("ft_strmapi 👎", 1);
+		ft_putendl_fd("❌ ft_strmapi", 1);
 	// =================================================================
 	char *striteri_result = ft_strdup(strmapi_str);
 	ft_striteri(striteri_result, &to_uppercase_ptr);
 	// ft_putstr_fd(striteri_result, 1);
 	if (!strcmp(striteri_result, strmapi_expected))
-		ft_putendl_fd("ft_striteri 👍", 1);
+		ft_putendl_fd("✅ ft_striteri", 1);
 	else
-		ft_putendl_fd("ft_striteri  👎", 1);
+		ft_putendl_fd("❌ ft_striteri", 1);
 	free(striteri_result);
+	// =================================================================
+	char *substr_expected = "mgrl39";
+	char const *substr_full = "Welcome everyone to mgrl39's Main";
+	char *substr_result = ft_substr(substr_full, 20, 6);
+	if (!ft_strncmp(substr_expected, substr_result, ft_strlen(substr_result)))
+		ft_putendl_fd("✅ ft_substr", 1);
+	else
+		ft_putendl_fd("❌ ft_substr", 1);
+	// =================================================================
+	char *split_result[] = {"Welcome", "everyone", "to", "mgrl39's", "Main", NULL};
+	char **split_returned = ft_split(substr_full, ' ');	
+
+	int	split_eval;
+	split_eval = 1;
+
+	split_eval &= compare_split_arrays(split_result, split_returned);
+	if (split_eval)
+		ft_putendl_fd("✅ ft_split", 1);
+	else
+		ft_putendl_fd("❌ ft_split", 1);
 }
 
+static void ft_limiter(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		if (ft_strlen(&argv[1][0]) != 1)
+		{
+			ft_putendl_fd("IMPORTANT=======================================", 1);
+			ft_putendl_fd("Only one character. \nExample:", 1);
+			ft_putstr_fd(argv[0], 1);
+			ft_putchar_fd(' ', 1);
+			ft_putendl_fd("a", 1);
+			return ;
+		}
+		ft_is_tester(argv[1][0], 1);
+		ft_to_tester(argv[1][0], 1);
+	}
+}
 int	main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		ft_putendl_fd("Only two args...", 1);
-		ft_putstr_fd("Example: ", 1);
-		ft_putstr_fd(argv[0], 1);
-		ft_putchar_fd(' ', 1);
-		ft_putendl_fd("a", 1);
-		return (0);
-	}
-	if (ft_strlen(&argv[1][0]) != 1)
-	{
-		ft_putendl_fd("Only one character. \nExample:", 1);
-		ft_putstr_fd(argv[0], 1);
-		ft_putchar_fd(' ', 1);
-		ft_putendl_fd("a", 1);
-		return (0);
-	}
-	ft_is_tester(argv[1][0], 1);
-	ft_to_tester(argv[1][0], 1);
+	ft_limiter(argc, argv);
 	ft_mem_tester();
 	ft_conv_tester();
 	ft_str_tester();
