6ba593515eafc1bc2b70bde59d664e50af460e0f
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-07-17 10:27:54 +0200

    Update ft_strlcpy.c

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -1,3 +1,4 @@
+```c
 #include "libft.h"
 
 /*
@@ -9,23 +10,20 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	len_src;
 	size_t	i;
+	size_t	src_len;
 
-	if (!dst || !src)
-		return (0);
-	len_src = 0;
-	while (src[len_src] != '\0')
-		len_src++;
+	src_len = ft_strlen(src);
+	i = 0;
 	if (size > 0)
 	{
-		i = 0;
-		while (src[i] != '\0' && i < (size - 1))
+		while (src[i] && i < (size - 1))
 		{
 			dst[i] = src[i];
 			i++;
 		}
 		dst[i] = '\0';
 	}
-	return (len_src);
+	return (src_len);
 }
+```
