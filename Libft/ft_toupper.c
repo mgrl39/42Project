70ca0fb35f76e41b0689f5b2d8ff1984197ca035
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-07-17 10:33:16 +0200

    Update ft_toupper.c

--- Libft/ft_toupper.c
+++ Libft/ft_toupper.c
@@ -1,12 +1,16 @@
+```c
 #include "libft.h"
 
-// ft_toupper function that converts a lowercase letter to uppercase.
-// Returns the uppercase equivalent of the input character 'c',
-// or the character itself if 'c' is not a lowercase letter.
+/*
+ * ft_toupper() function that converts a lowercase letter to uppercase.
+ * Returns the uppercase equivalent of the input character 'c',
+ * or the character itself if 'c' is not a lowercase letter.
+ */
 
 int	ft_toupper(int c)
 {
-	if (c >= 97 && c <= 122)
-		c -= 32;
+	if (c >= 'a' && c <= 'z')
+		c -= 'a' - 'A';
 	return (c);
 }
+```
