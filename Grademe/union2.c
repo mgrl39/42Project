c28666ccb149ba34b923023c09e2b061a08d2ac9
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-09-21 12:16:38 +0200

    Create union2.c

---
+++ Grademe/union2.c
@@ -0,0 +1,40 @@
+
+#include <unistd.h>
+
+void	ft_union(char *s1, char *s2)
+{
+	int	i;
+	char	appeared[256];
+
+	i = 0;
+	while (i < 256)
+		appeared[i++] = 0;
+	i = 0;
+	while (s1[i])
+	{
+		if (appeared[(unsigned char)s1[i]] == 0)
+		{
+			appeared[(unsigned char)s1[i]] = 1;
+			write(1, &s1[i], 1);
+		}
+		i++;
+	}
+	i = 0;
+	while (s2[i])
+	{
+		if (appeared[(unsigned char)s2[i]] == 0)
+		{
+			appeared[(unsigned char)s2[i]] = 1;
+			write(1, &s2[i], 1);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 3)
+		ft_union(argv[1], argv[2]);
+	write(1, "\n", 1);
+	return (0);
+}
